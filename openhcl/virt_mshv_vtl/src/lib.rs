1043a8ee97442d349f742861b4044f7318a34e35
Ben Aram <b-bearam@microsoft.com>
2026-04-02 16:15:51 +0100

    cca: allow(dead_code) to ignore warning

--- openhcl/virt_mshv_vtl/src/lib.rs
+++ openhcl/virt_mshv_vtl/src/lib.rs
@@ -1664,7 +1664,7 @@ pub struct UhProtoPartition<'a> {
     cpuid: virt::CpuidLeafSet,
     #[cfg(guest_arch = "aarch64")]
     realm_config: RsiRealmConfig,
-    #[warn(dead_code)]
+    #[allow(dead_code)]
     addresses: Addresses,
 }
 
