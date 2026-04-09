9829a7c4b522bae3dcc1ee24272e9bfe088991f7
Ben Aram <b-bearam@microsoft.com>
2026-04-09 11:55:19 +0100

    cca: changing the error from SetRegistersR to SetRegisters

--- openhcl/virt_mshv_vtl/src/processor/mshv/arm64.rs
+++ openhcl/virt_mshv_vtl/src/processor/mshv/arm64.rs
@@ -859,7 +859,7 @@ impl UhVpStateAccess<'_, '_, HypervisorBackedArm64> {
         self.vp
             .runner
             .set_vp_registers(self.vtl, names.iter().copied().zip(values))
-            .map_err(vp_state::Error::SetRegistersR)?;
+            .map_err(vp_state::Error::SetRegisters)?;
         Ok(())
     }
 
@@ -875,7 +875,7 @@ impl UhVpStateAccess<'_, '_, HypervisorBackedArm64> {
         self.vp
             .runner
             .get_vp_registers(self.vtl, &names, &mut values)
-            .map_err(vp_state::Error::GetRegistersR)?;
+            .map_err(vp_state::Error::GetRegisters)?;
 
         regs.set_values(values.into_iter());
         Ok(regs)
