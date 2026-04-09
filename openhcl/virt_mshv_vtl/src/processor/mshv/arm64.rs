a1346f20c42a2d25db6d7ad176b3ee84dc4cd934
Ben Aram <b-bearam@microsoft.com>
2026-04-09 16:27:16 +0100

    cca: changed error value back just for now to keep the code working

--- openhcl/virt_mshv_vtl/src/processor/mshv/arm64.rs
+++ openhcl/virt_mshv_vtl/src/processor/mshv/arm64.rs
@@ -859,7 +859,7 @@ impl UhVpStateAccess<'_, '_, HypervisorBackedArm64> {
         self.vp
             .runner
             .set_vp_registers(self.vtl, names.iter().copied().zip(values))
-            .map_err(ioctl::register::SetRegError::Ioctl)?;
+            .map_err(vp_state::Error::SetRegistersR)?;
         Ok(())
     }
 
@@ -875,7 +875,7 @@ impl UhVpStateAccess<'_, '_, HypervisorBackedArm64> {
         self.vp
             .runner
             .get_vp_registers(self.vtl, &names, &mut values)
-            .map_err(ioctl::register::GetRegError::Ioctl)?;
+            .map_err(vp_state::Error::GetRegistersR)?;
 
         regs.set_values(values.into_iter());
         Ok(regs)
