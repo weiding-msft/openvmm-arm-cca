weiding-msft/openvmm

flowey: add cca-fvp-test option to install and build shrinkwrap

flowey/flowey_hvlite/src/pipelines/mod.rs
@@ -4,6 +4,8 @@
 use flowey::pipeline::prelude::*;
 use restore_packages::RestorePackagesCli;
 use vmm_tests::VmmTestsCli;
+use cca_fvp::CcaFvpCli;
+use cca_fvp_test::CcaFvpTestCli;
 
 pub mod build_docs;
 pub mod build_igvm;
@@ -11,6 +13,8 @@ pub mod checkin_gates;
 pub mod custom_vmfirmwareigvm_dll;
 pub mod restore_packages;
 pub mod vmm_tests;
+pub mod cca_fvp;
+pub mod cca_fvp_test;
 
 #[derive(clap::Subcommand)]
 #[expect(clippy::large_enum_variant)]
@@ -31,6 +35,12 @@ pub enum OpenvmmPipelines {
 
     /// Install tools needed to build OpenVMM
     RestorePackages(RestorePackagesCli),
+
+    /// Build and run CCA FVP via Shrinkwrap (local)
+    CcaFvp(CcaFvpCli),
+
+    /// Build+run CCA FVP and validate logs/markers (local)
+    CcaFvpTest(CcaFvpTestCli),
 
     /// Build and run VMM tests
     VmmTests(VmmTestsCli),
@@ -60,6 +70,8 @@ impl IntoPipeline for OpenvmmPipelines {
                 OpenvmmPipelinesCi::BuildDocs(cmd) => cmd.into_pipeline(pipeline_hint),
             },
             OpenvmmPipelines::RestorePackages(cmd) => cmd.into_pipeline(pipeline_hint),
+            OpenvmmPipelines::CcaFvp(cmd) => cmd.into_pipeline(pipeline_hint),
+            OpenvmmPipelines::CcaFvpTest(cmd) => cmd.into_pipeline(pipeline_hint),
             OpenvmmPipelines::VmmTests(cmd) => cmd.into_pipeline(pipeline_hint),
         }
     }

weiding-msft/openvmm-arm-cca

flowey/flowey_hvlite/src/pipelines/mod.rs
@@ -4,6 +4,8 @@
 use flowey::pipeline::prelude::*;
 use restore_packages::RestorePackagesCli;
 use vmm_tests_run::VmmTestsRunCli;
+use cca_fvp::CcaFvpCli;
+use cca_fvp_test::CcaFvpTestCli;
 
 pub mod build_docs;
 pub mod build_igvm;
@@ -12,6 +14,8 @@ pub mod checkin_gates;
 pub mod custom_vmfirmwareigvm_dll;
 pub mod restore_packages;
 pub mod vmm_tests_run;
+pub mod cca_fvp;
+pub mod cca_fvp_test;
 
 #[derive(clap::Subcommand)]
 #[expect(clippy::large_enum_variant)]
@@ -33,6 +37,12 @@ pub enum OpenvmmPipelines {
 
     /// Install tools needed to build OpenVMM
     RestorePackages(RestorePackagesCli),
+
+    /// Build and run CCA FVP via Shrinkwrap (local)
+    CcaFvp(CcaFvpCli),
+
+    /// Build+run CCA FVP and validate logs/markers (local)
+    CcaFvpTest(CcaFvpTestCli),
 
     /// Build and run VMM tests with automatic artifact discovery
     VmmTestsRun(VmmTestsRunCli),
@@ -63,7 +73,7 @@ impl IntoPipeline for OpenvmmPipelines {
                 OpenvmmPipelinesCi::BuildDocs(cmd) => cmd.into_pipeline(pipeline_hint),
             },
             OpenvmmPipelines::RestorePackages(cmd) => cmd.into_pipeline(pipeline_hint),
-            OpenvmmPipelines::VmmTestsRun(cmd) => cmd.into_pipeline(pipeline_hint),
+            OpenvmmPipelines::VmmTests(cmd) => cmd.into_pipeline(pipeline_hint),
         }
     }
 }