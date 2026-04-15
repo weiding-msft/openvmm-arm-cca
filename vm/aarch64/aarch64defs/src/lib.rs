442acf73a18b73453d3b3acab659ad8ac07646c6
Ben Aram <b-bearam@microsoft.com>
2026-04-15 11:24:46 +0100

    cca: debugging esrel2

--- vm/aarch64/aarch64defs/src/lib.rs
+++ vm/aarch64/aarch64defs/src/lib.rs
@@ -87,7 +87,7 @@ impl EsrEl2 {
         println!("il: {}", self.il());
         println!("ec: {}", self.ec());
         println!("iss2: {}", self.iss2());
-        print!("rsvd: {}", self._rsvd());
+        print!("rsvd: {}", self.rsvd());
         self.wnr() != false
     }
 
