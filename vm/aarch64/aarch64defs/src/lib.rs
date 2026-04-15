df57b4af2ba2731ac97915157944ec7865dc6c55
Ben Aram <b-bearam@microsoft.com>
2026-04-15 14:30:02 +0100

    cca: debugging esrel2 struct

--- vm/aarch64/aarch64defs/src/lib.rs
+++ vm/aarch64/aarch64defs/src/lib.rs
@@ -85,10 +85,8 @@ pub struct EsrEl2 {
 impl EsrEl2 {
     pub fn is_write(&self) -> bool {
         // The WNR bit is set for writes, not reads.
-        let temp_iss: u32 = (self.lower_iss() as u32) | ((self.wnr() as u32) << 6) | ((self.mid() as u32) << 7);
         println!("lower_iss: {}", self.lower_iss());
         println!("mid: {}", self.mid_iss());
-        println!("combination to iss: {}", temp_iss);
         println!("is write {}", self.wnr());
         println!("il: {}", self.il());
         println!("ec: {}", self.ec());
