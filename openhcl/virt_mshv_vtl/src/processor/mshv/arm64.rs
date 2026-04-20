618cacf7ffeb336fbe99c77537357e8854a74513
Ben Aram <b-bearam@microsoft.com>
2026-04-20 14:40:42 +0100

    cca: getting rid of Set/GetRegisterR variants and using Se/GettRegisters variants. Also changing the error type in Set/GetRegisters to Set/GetRegError since this is preferred.

--- openhcl/virt_mshv_vtl/src/processor/mshv/arm64.rs
+++ openhcl/virt_mshv_vtl/src/processor/mshv/arm64.rs
@@ -856,7 +856,7 @@ impl UhVpStateAccess<'_, '_, HypervisorBackedArm64> {
         self.vp
             .runner
             .set_vp_registers(self.vtl, names.iter().copied().zip(values))
-            .map_err(vp_state::Error::SetRegistersR)?;
+            .map_err(vp_state::Error::SetRegisters)?;
         Ok(())
     }
 
@@ -872,7 +872,7 @@ impl UhVpStateAccess<'_, '_, HypervisorBackedArm64> {
         self.vp
             .runner
             .get_vp_registers(self.vtl, &names, &mut values)
-            .map_err(vp_state::Error::GetRegistersR)?;
+            .map_err(vp_state::Error::GetRegisters)?;
 
         regs.set_values(values.into_iter());
         Ok(regs)
--- openhcl/virt_mshv_vtl/src/processor/vp_state.rs
+++ openhcl/virt_mshv_vtl/src/processor/vp_state.rs
@@ -22,19 +22,19 @@ impl<'a, 'p, T: Backing> UhVpStateAccess<'a, 'p, T> {
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("failed to set registers")]
-    SetRegisters(#[source] hcl::ioctl::Error),
+    SetRegisters(#[source] SetRegError),
     #[error("failed to get registers")]
-    GetRegisters(#[source] hcl::ioctl::Error),
+    GetRegisters(#[source] GetRegError),
     #[error("the value for setting efer {0} is invalid, {1}")]
     SetEfer(u64, &'static str),
     #[error("'{0}' state is not implemented yet")]
     Unimplemented(&'static str),
     #[error("failed to set apic base MSR")]
     InvalidApicBase(#[source] virt_support_apic::InvalidApicBase),
-    #[error("failed to set registers")]
-    SetRegistersR(#[source] SetRegError),
-    #[error("failed to get registers")]
-    GetRegistersR(#[source] GetRegError),
+    // #[error("failed to set registers")]
+    // SetRegistersR(#[source] SetRegError),
+    // #[error("failed to get registers")]
+    // GetRegistersR(#[source] GetRegError),
 }
 
 // /// temp - just to get round some error compatibilities
