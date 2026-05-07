9238ff2c6020b8cb569a776226e9153d53a9bdb6
Ben Aram <b-bearam@microsoft.com>
2026-05-07 15:18:17 +0100

    cca: removing comments and adding _ prefix

--- openhcl/underhill_mem/src/lib.rs
+++ openhcl/underhill_mem/src/lib.rs
@@ -363,14 +363,10 @@ impl MemoryAcceptor {
                     })
             }
             GpaVtlPermissions::Cca(_index) => {
-                // TODO: CCA: call new ioctl to set perms index
-                // TODO: CCA: next
+                // CCA: call new ioctl to set perms index
                 self.mshv_vtl
                     .cca_set_mem_perm(&range)
-                    .map_err(|err| ApplyVtlProtectionsError::Cca { range, vtl: vtl.into() })
-                // todo!(
-                //     "Apply CCA permissions for vtl {vtl:?} on range {range:?} with index {_index:?}"
-                // );
+                    .map_err(|_err| ApplyVtlProtectionsError::Cca { range, vtl: vtl.into() })
             }
         }
     }
