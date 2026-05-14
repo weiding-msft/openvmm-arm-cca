8f6cf7aee0b13c144f9f0dd5d7774bb5574fff3e
Ben Aram <b-bearam@microsoft.com>
2026-05-14 15:59:24 +0100

    cca: const _: () = assert!

--- vm/aarch64/rsi/src/lib.rs
+++ vm/aarch64/rsi/src/lib.rs
@@ -118,7 +118,7 @@ pub struct cca_rsi_plane_run {
     pub pad9: [u8; 0x800 - size_of::<cca_rsi_plane_exit>()],
 }
 
-static_assertions::const_assert_eq!(0x1000, size_of::<cca_realm_config>());
-static_assertions::const_assert_eq!(0x300, size_of::<cca_rsi_plane_entry>());
-static_assertions::const_assert_eq!(0x400, size_of::<cca_rsi_plane_exit>());
-static_assertions::const_assert_eq!(0x1000, size_of::<cca_rsi_plane_run>());
+const _: () = assert!(size_of::<cca_realm_config>() == 0x1000);
+const _: () = assert!(size_of::<cca_rsi_plane_entry>() == 0x300);
+const _: () = assert!(size_of::<cca_rsi_plane_exit>() == 0x400);
+const _: () = assert!(size_of::<cca_rsi_plane_run>() == 0x1000);
